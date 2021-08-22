e98b689d3eb4bb7fbc7604c307a6059174c48483
nanigashi <26033486+ynanigashi@users.noreply.github.com>
2021-08-22 17:33:02 +0900

    change session to

--- application.py
+++ application.py
@@ -37,11 +37,10 @@ def after_request(response):
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# Configure session to use secret_key
+if not os.environ.get("SECRET_KEY_STR"):
+    raise RuntimeError("SECRET_KEY_STR not set")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY_STR')
 
 
 # Make sure API key is set
